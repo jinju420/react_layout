16514889b89726e082c0e9a52e4b5ecfa174a903
jinju420 <ljjshlove@nate.com>
2023-03-03 15:27:22 +0900

    vids swiper주석추가정리

--- src/components/main/Vids.js
+++ src/components/main/Vids.js
@@ -1,6 +1,6 @@
-import { memo, useRef, useState } from 'react';
+import { memo, useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination, Navigation } from 'swiper';
 import { EffectCoverflow } from 'swiper';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -11,41 +11,29 @@ import 'swiper/css';
 import Modal from '../common/Modal';
 // import 'swiper/css/effect-coverflow';
 
-function BtnRolling() {
-	const swiper = useSwiper();
-	const btnRun = useRef(null);
-	const btnPause = useRef(null);
-	return (
-		<nav className='controls'>
-			<FontAwesomeIcon
-				className='on'
-				ref={btnRun}
-				icon={faPlay}
-				onClick={() => {
-					if (!swiper.autoplay.paused) return;
-					swiper.autoplay.run();
-					btnRun.current.classList.add('on');
-					btnPause.current.classList.remove('on');
-				}}
-			/>
-			<FontAwesomeIcon
-				ref={btnPause}
-				icon={faPause}
-				onClick={() => {
-					if (swiper.autoplay.paused) return;
-					swiper.autoplay.pause();
-					btnRun.current.classList.remove('on');
-					btnPause.current.classList.add('on');
-				}}
-			/>
-		</nav>
-	);
-}
-
 function Vids() {
 	const Vids = useSelector((store) => store.youtube.data);
 	const open = useRef(null);
+	//Swiper 컴포넌트에서 생성되는 인스턴스를 담을 객체
+	const [Instance, setInstance] = useState(null);
 	const [Index, setIndex] = useState(0);
+	const btnStart = useRef(null);
+	const btnStop = useRef(null);
+
+	useEffect(() => {
+		//pagination, prev, next버튼을 처음 마운트시 한번 담고
+		const btnPagination = document.querySelector('.swiper-pagination');
+		const btnPrev = document.querySelector('.swiper-button-prev');
+		const btnNext = document.querySelector('.swiper-button-next');
+
+		//위의 버튼을 클릭시 정지버튼 활성화
+		[btnPagination, btnPrev, btnNext].map((el) => {
+			el.addEventListener('click', () => {
+				btnStart.current.classList.remove('on');
+				btnStop.current.classList.add('on');
+			});
+		});
+	}, []);
 	return (
 		<>
 			<section id='vids' className='myScroll'>
@@ -77,8 +65,34 @@ function Vids() {
 						modifier: 1,
 						slideShadows: false,
 					}}
+					onSwiper={(swiper) => setInstance(swiper)}
 				>
-					<BtnRolling />
+					<nav className='controls'>
+						<FontAwesomeIcon
+							className='on'
+							ref={btnStart}
+							icon={faPlay}
+							onClick={() => {
+								//pagination, 좌우버튼 클릭시 일지 정지가 되는 것이 아닌 autoplay기능 자체가 비활성화 됨
+								//현재 자동롤링 동작 유무는 swiper.autoplay.running값으로 확인
+								//자동롤링 시작, 정지 함수도 start, stop으로 변경
+								if (Instance.autoplay.running) return;
+								Instance.autoplay.start();
+								btnStart.current.classList.add('on');
+								btnStop.current.classList.remove('on');
+							}}
+						/>
+						<FontAwesomeIcon
+							ref={btnStop}
+							icon={faPause}
+							onClick={() => {
+								if (!Instance.autoplay.running) return;
+								Instance.autoplay.stop();
+								btnStart.current.classList.remove('on');
+								btnStop.current.classList.add('on');
+							}}
+						/>
+					</nav>
 					{Vids.map((vid, idx) => {
 						if (idx >= 6) return null;
 						return (
@@ -89,6 +103,9 @@ function Vids() {
 										onClick={() => {
 											setIndex(idx);
 											open.current.setOpen();
+											Instance.autoplay.stop();
+											document.querySelector('.fa-play').classList.remove('on');
+											document.querySelector('.fa-pause').classList.add('on');
 										}}
 									>
 										<img src={vid.snippet.thumbnails.maxres.url} alt={vid.snippet.title} />
