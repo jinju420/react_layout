b323e0b5304e154f4d2145be645baad4c2583a8b
jinju420 <ljjshlove@nate.com>
2023-03-31 23:21:11 +0900

    detail/list컴포넌트생성

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -2,62 +2,53 @@ import { memo, useEffect, useRef, useState } from 'react';
 import axios from 'axios';
 
 function Visual() {
-	const [Img, setImg] = useState([]);
-	const list = useRef(null);
-	const prev = useRef(null);
-	const next = useRef(null);
-	const li = useRef(null);
-	const inner = useRef(true);
-	// const enableClick = useRef(true);
+	const [Info, setInfo] = useState([]);
+	const getData = async () => {
+		const data = await axios.get(process.env.PUBLIC_URL + '/DB/data.json');
+		setInfo(data.data.info);
+	};
 
-	useEffect(() => {
-		axios.get(`${process.env.PUBLIC_URL}/DB/swiper.json`).then((json) => {
-			setImg(json.data.swiper);
-		});
+	useEffect(() => getData(), []);
+	// const [Img, setImg] = useState([]);
+	// const list = useRef(null);
+	// const prev = useRef(null);
+	// const next = useRef(null);
+	// const li = useRef(null);
+	// const inner = useRef(true);
 
-		prev.current.addEventListener('click', () => {
-			list.current.append(list.current.firstElementChild);
-			prev.current.classList.add('on');
-			setTimeout(() => prev.current.classList.remove('on'), 500);
-		});
+	// useEffect(() => {
+	// 	axios.get(`${process.env.PUBLIC_URL}/DB/swiper.json`).then((json) => {
+	// 		setImg(json.data.swiper);
+	// 	});
 
-		next.current.addEventListener('click', () => {
-			list.current.prepend(list.current.lastElementChild);
-			next.current.classList.add('on');
-			setTimeout(() => next.current.classList.remove('on'), 500);
-		});
-	}, []);
+	// 	prev.current.addEventListener('click', () => {
+	// 		list.current.append(list.current.firstElementChild);
+	// 		prev.current.classList.add('on');
+	// 		setTimeout(() => prev.current.classList.remove('on'), 500);
+	// 	});
+
+	// 	next.current.addEventListener('click', () => {
+	// 		list.current.prepend(list.current.lastElementChild);
+	// 		next.current.classList.add('on');
+	// 		setTimeout(() => next.current.classList.remove('on'), 500);
+	// 	});
+	// }, []);
 
 	return (
 		<div id='visual' className='myScroll'>
 			<div className='inner'>
-				<ul className='list' ref={list}>
-					{Img.map((img, idx) => {
-						// const [Txt, setTxt] = img.title.split(' ');
+				<ul className='list'>
+					{/* {Img.map((img, idx) => {
 						return (
 							<li key={idx} ref={li}>
 								<div className='inner' ref={inner}>
 									<img src={`${process.env.PUBLIC_URL}/img/swiper/${img.pic}`} alt={img.pic} />
 									<h2>{img.title}</h2>
-									{/* <h2>
-										{Txt}
-										<br />
-										{setTxt}
-									</h2> */}
 								</div>
 							</li>
 						);
-					})}
+					})} */}
 				</ul>
-
-				<nav className='navi'>
-					<p className='prev' ref={prev}>
-						<span></span>
-					</p>
-					<p className='next' ref={next}>
-						<span></span>
-					</p>
-				</nav>
 			</div>
 		</div>
 	);
